68a71dea19ead2f29c1d160d9b011bba54da0348
LuNeder <luana@luana.dev.br>
2022-03-03 11:12:47 -0300

    Please enter the commit message for your changes. Lines starting

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
+  # bot.py
+  # Copyright 2022 Luana Neder
+  #
+  # This work may be distributed and/or modified under the
+  # conditions of the LaTeX Project Public License, either version 1.3
+  # of this license or (at your option) any later version.
+  # The latest version of this license is in
+  #   http://www.latex-project.org/lppl.txt
+  # and version 1.3 or later is part of all distributions of LaTeX
+  # version 2005/12/01 or later.
+  #
+  # This work has the LPPL maintenance status `maintained'.
+  #
+  # The Current Maintainer of this work is Luana Neder
+  #
+  # This work consists of the file bot.py
+
+
+
 # //just testing for now probably shouldnt mind reading this lol
 
 import tweepy
